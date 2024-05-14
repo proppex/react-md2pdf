d6215b36099be27cd1440f1e61dabc8e5e55ac00
Thiamath <thiamath@gmail.com>
2024-05-14 17:28:08 +0200

    Reduce the cluttering of the recursive function and clean up unused files and functions.

--- server-test/app/pdf/Viewer.tsx
+++
@@ -1,5 +0,0 @@
-import { Document } from "react-pdf";
-
-export default function Viewer() {
-  return <Document />;
-}
--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,16 @@ import * as md from "mdast";
 import { styles } from "./styles";
 
 interface Props extends ViewProps {
-  message: string;
+  md: string;
 }
 
-const Markdown = (props: Props) => {
-  return <View>{markdownToReactPDF(props.message)}</View>;
-};
+/**
+ * Renders a Markdown message as a React PDF component.
+ *
+ * @param props - The component props.
+ * @returns The rendered React PDF component.
+ */
+const Markdown = (props: Props) => <View>{markdownToReactPDF(props.md)}</View>;
 
 export default Markdown;
 
@@ -22,9 +26,6 @@ const markdownToReactPDF = (markdown: string) => {
     (node as md.Root).children !== undefined &&
     (node as md.Root).type === "root";
 
-  const isParent = (node: md.Node): node is md.Parent =>
-    (node as md.Parent).children !== undefined;
-
   const isHeading = (node: md.Node): node is md.Heading =>
     (node as md.Heading).depth !== undefined &&
     (node as md.Heading).type === "heading";
@@ -32,93 +33,90 @@ const markdownToReactPDF = (markdown: string) => {
   const isParagraph = (node: md.Node): node is md.Paragraph =>
     (node as md.Paragraph).type === "paragraph";
 
+  const isEmphasis = (node: md.Node): node is md.Emphasis =>
+    (node as md.Emphasis).type === "emphasis";
+
   const isLiteral = (node: md.Node): node is md.Literal =>
     (node as md.Literal).value !== undefined;
 
   const isImage = (node: md.Node): node is md.Image =>
     (node as md.Image).type === "image";
 
-  const processNode = (node: md.Node, style?: Style): JSX.Element => {
-    // console.log(node);
-    if (isParent(node)) {
-      if (isRoot(node))
-        return (
-          <View
-            key={node.position?.start.offset}
-            style={{ fontFamily: "Times-Roman" }}
-          >
-            {node.children.map((child) => processNode(child))}
-          </View>
-        );
+  const processRoot = (node: md.Root): JSX.Element => (
+    <View
+      key={node.position?.start.offset}
+      style={{ fontFamily: "Times-Roman" }}
+    >
+      {node.children.map((child) => processNode(child))}
+    </View>
+  );
 
-      if (isHeading(node)) {
-        if (node.depth === 1 || node.depth === 2 || node.depth === 3) {
-          const headerStyle =
-            node.depth === 1
-              ? styles.header_1
-              : node.depth === 2
-              ? styles.header_2
-              : styles.header_3;
-          return (
-            <View key={node.position?.start.offset}>
-              {node.children.map((child: md.Node) =>
-                processNode(child, { ...headerStyle, ...style })
-              )}
-            </View>
-          );
-        }
-        return (
-          <View key={node.position?.start.offset}>
-            {node.children.map((child: md.Node) =>
-              processNode(child, { ...styles.paragraph, ...style })
-            )}
-          </View>
-        );
-      } else if (isParagraph(node)) {
-        return (
-          <View>
-            <Text key={node.position?.start.offset} style={styles.paragraph}>
-              {node.children.map((child) => processNode(child, style))}
-            </Text>
-          </View>
-        );
-      } else if (node.type === "emphasis") {
-        return (
-          <Text key={node.position?.start.offset}>
-            {node.children.map((child) =>
-              processNode(child, { ...styles.strong, ...style })
-            )}
-          </Text>
-        );
-      }
-    } else if (isLiteral(node)) {
+  const processHeading = (node: md.Heading, style?: Style): JSX.Element => {
+    if (node.depth === 1 || node.depth === 2 || node.depth === 3) {
+      const headerStyle =
+        node.depth === 1
+          ? styles.header_1
+          : node.depth === 2
+          ? styles.header_2
+          : styles.header_3;
       return (
-        <Text key={node.position?.start.offset} style={style}>
-          {node.value}
-        </Text>
-      );
-    } else if (isImage(node)) {
-      return (
-        <Image
-          key={node.position?.start.offset}
-          style={styles.imageCompanyLogo}
-          // src={{
-          //   uri: node.url,
-          //   method: "GET",
-          //   headers: {
-          //     Origin: "sdo.gsfc.nasa.gov",
-          //   },
-          //   body: "",
-          // }}
-          src={() => fetch(node.url).then((res) => res.blob())}
-        />
+        <View key={node.position?.start.offset}>
+          {node.children.map((child: md.Node) =>
+            processNode(child, { ...headerStyle, ...style })
+          )}
+        </View>
       );
     }
+    return (
+      <View key={node.position?.start.offset}>
+        {node.children.map((child: md.Node) =>
+          processNode(child, { ...styles.paragraph, ...style })
+        )}
+      </View>
+    );
+  };
+
+  const processParagraph = (node: md.Paragraph, style?: Style): JSX.Element => (
+    <View>
+      <Text key={node.position?.start.offset} style={styles.paragraph}>
+        {node.children.map((child) => processNode(child, style))}
+      </Text>
+    </View>
+  );
+
+  const processEmphasis = (node: md.Emphasis, style?: Style): JSX.Element => (
+    <Text key={node.position?.start.offset}>
+      {node.children.map((child) =>
+        processNode(child, { ...styles.strong, ...style })
+      )}
+    </Text>
+  );
+
+  const processLiteral = (node: md.Literal, style?: Style): JSX.Element => (
+    <Text key={node.position?.start.offset} style={style}>
+      {node.value}
+    </Text>
+  );
+
+  const processImage = (node: md.Image, style?: Style): JSX.Element => (
+    <Image
+      key={node.position?.start.offset}
+      style={styles.imageCompanyLogo}
+      src={() => fetch(node.url).then((res) => res.blob())}
+    />
+  );
+
+  const processNode = (node: md.Node, style?: Style): JSX.Element => {
+    // console.log(node);
+    if (isRoot(node)) return processRoot(node);
+    if (isHeading(node)) return processHeading(node, style);
+    if (isParagraph(node)) return processParagraph(node, style);
+    if (isEmphasis(node)) return processEmphasis(node, style);
+    if (isLiteral(node)) return processLiteral(node, style);
+    if (isImage(node)) return processImage(node, style);
   };
 
   const tree = remark().use(parse).parse(markdown);
 
-  const element = processNode(tree);
-  console.log(element);
-  return element;
+  return processNode(tree);
 };
