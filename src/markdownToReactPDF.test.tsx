f2091476d2530c52efcfb7a5db70367e12155d7c
Marcos Garcia <thiamath@gmail.com>
2024-05-20 21:04:09 +0200

    Working tests with vitest

--- src/markdownToReactPDF.test.tsx
+++ src/markdownToReactPDF.test.tsx
@@ -1,53 +1,52 @@
+import { describe, expect, test } from "vitest";
+
 import markdownToReactPDF from "./markdownToReactPDF";
 
 describe("markdownToReactPDF", () => {
-  it("should render a root node with children", () => {
+  test("should render a root node with children", () => {
     const markdown = `
-      # Heading 1
-      ## Heading 2
-      ### Heading 3
-      Paragraph
+# Heading 1
+## Heading 2
+### Heading 3
+Paragraph
     `;
     const result = markdownToReactPDF(markdown);
-    expect(result).toBeDefined();
+
+    expect(result).toMatchSnapshot();
   });
 
-  // it("should render a heading node with style", () => {
-  //   const markdown = `
-  //     # Heading 1
-  //     ## Heading 2
-  //     ### Heading 3
-  //   `;
-  //   const result = markdownToReactPDF(markdown);
-  //   const container = render(result);
-  //   expect(container).toMatchSnapshot();
-  // });
+  test("should render a heading node with style", () => {
+    const markdown = `
+# Heading 1
+## Heading 2
+### Heading 3
+    `;
+    const result = markdownToReactPDF(markdown);
+    expect(result).toMatchSnapshot();
+  });
 
-  // it("should render a paragraph node with style", () => {
-  //   const markdown = `
-  //     Paragraph
-  //   `;
-  //   const result = markdownToReactPDF(markdown);
-  //   const container = render(result);
-  //   expect(container).toMatchSnapshot();
-  // });
+  test("should render a paragraph node with style", () => {
+    const markdown = `
+Paragraph
+    `;
+    const result = markdownToReactPDF(markdown);
+    expect(result).toMatchSnapshot();
+  });
 
-  // it("should render an emphasis node with style", () => {
-  //   const markdown = `
-  //     *Emphasis*
-  //     **Strong**
-  //   `;
-  //   const result = markdownToReactPDF(markdown);
-  //   const container = render(result);
-  //   expect(container).toMatchSnapshot();
-  // });
+  test("should render an emphasis node with style", () => {
+    const markdown = `
+*Emphasis*
+**Strong**
+    `;
+    const result = markdownToReactPDF(markdown);
+    expect(result).toMatchSnapshot();
+  });
 
-  // it("should render an image node with style", () => {
-  //   const markdown = `
-  //     ![Company Logo](https://example.com/logo.png)
-  //   `;
-  //   const result = markdownToReactPDF(markdown);
-  //   const container = render(result);
-  //   expect(container).toMatchSnapshot();
-  // });
+  test("should render an image node with style", () => {
+    const markdown = `
+![Company Logo](https://example.com/logo.png)
+    `;
+    const result = markdownToReactPDF(markdown);
+    expect(result).toMatchSnapshot();
+  });
 });
